04a1750feb684c36604927633d450e053d5b2e87
Swenne Offerhaus <swennehacks@gmail.com>
2020-03-24 22:25:38 +0100

    del del del

--- printf.h
+++ printf.h
@@ -13,40 +13,50 @@
 #ifndef PRINTF_H
 # define PRINTF_H
 
+# define NOFLAG 0
+# define MINUS -1
+# define ZERO 1
+
 # include <stdarg.h>
 # include <stdio.h>
 # include <unistd.h>
-# include "libft/libft.h"
+# include "../libft/libft.h"
 
-va_list	g_ap;
+va_list	g_argument;
 int		g_return;
 int		g_flag;
-int		g_minfield;
 int		g_period;
+int		g_minfield;
 int		g_maxfield;
-char	g_argument;
+char	g_conversion;
 
 int		ft_vabs(int nbr);
 int		ft_atoi(const char *str);
-int		ft_isdigit_signed(int c);
 size_t	ft_strlen(const char *str);
-void	init_globalv(void);
-int		ft_calcnbr(int nbr, unsigned long binf, unsigned long bsup);
+size_t	ft_lenbase(long long nbr, int base);
+void	ft_puthexa(unsigned long n, char c);
+void	ft_putocta(unsigned long n);
+void	ft_putnbr(int n);
 void	ft_putchar(char c);
 void	ft_putstr(char *str);
-void	ft_putnbr(int n);
-int		ft_putchar_while(char c, int i);
+int		ft_putlen(char c, int len);
+int		ft_isdigit_signed(int c);
+/*
+**      My printf Functions
+*/
+int		ft_printf(const char *str, ...);
+int		ft_percentage(const char *str);
+int		ft_flags(const char *str);
 void	ft_c_argument(void);
 void	ft_s_argument(void);
 void	ft_d_argument(void);
-int		ft_checkpercent(const char *str);
-int		ft_printf(const char *str, ...);
-int		ft_checkflags(const char *str);
-int		ft_checkmaxfield(const char *str);
-int		ft_checkperiod_maxfield(const char *str);
-int		ft_go_to_argument(char g_argument);
-int		ft_checkminfield(const char *str);
-int		ft_checkargument(const char *str);
-int		ft_check_after_percent(const char *str);
+void	ft_x_argument(void);
+void	init_globalv(void);
+int		ft_maxfield(const char *str);
+int		ft_precision(const char *str);
+int		ft_conversion(char g_argument);
+int		ft_minfield(const char *str);
+int		ft_argument(const char *str);
+int		ft_after_percent(const char *str);
 
 #endif
