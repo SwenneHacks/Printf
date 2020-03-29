05fc5830be37275d0c3418719987ac52cebef7aa
Swenne Offerhaus <swennehacks@gmail.com>
2020-03-29 23:28:00 +0200

    updated globals

--- printf.h
+++ printf.h
@@ -13,9 +13,10 @@
 #ifndef PRINTF_H
 # define PRINTF_H
 
-# define NOFLAG 0
-# define MINUS -1
-# define ZERO 1
+# define MINUS '-'
+# define SPACE ' '
+# define ZERO 'O'
+# define NOFLAG '0'
 
 # include <stdarg.h>
 # include <stdio.h>
@@ -24,13 +25,13 @@
 
 va_list	g_argument;
 int		g_return;
-int		g_flag;
+char	g_flag;
 int		g_period;
-int		g_minfield;
-int		g_maxfield;
+int		g_width;
+int		g_precision;
 char	g_conversion;
 
-int		ft_vabs(int nbr);
+int		ft_sign(int nbr);
 int		ft_atoi(const char *str);
 size_t	ft_strlen(const char *str);
 size_t	ft_lenbase(long long nbr, int base);
@@ -46,19 +47,19 @@ int		ft_isdigit_signed(int c);
 */
 int		ft_printf(const char *str, ...);
 int		ft_percentage(const char *str);
+int		ft_after_percent(const char *str);
 int		ft_flags(const char *str);
+void	init_globalv(void);
+int		ft_width(const char *str);
+int		ft_precision(const char *str);
+int		ft_asterik(const char *str);
+int		ft_conversion(char g_argument);
+int		ft_argument(const char *str);
 void	ft_c_argument(void);
 void	ft_s_argument(void);
 void	ft_d_argument(void);
 void	ft_x_argument(void);
 void	ft_p_argument(void);
 void	ft_u_argument(void);
-void	init_globalv(void);
-int		ft_maxfield(const char *str);
-int		ft_precision(const char *str);
-int		ft_conversion(char g_argument);
-int		ft_minfield(const char *str);
-int		ft_argument(const char *str);
-int		ft_after_percent(const char *str);
 
 #endif
