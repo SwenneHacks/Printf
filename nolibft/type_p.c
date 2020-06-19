ed00f14822732fe5c85d21dae382445621a915b0
Swenne Offerhaus <swennehacks@gmail.com>
2020-06-19 17:05:30 +0200

    [added folder] nolibft

---
+++ nolibft/type_p.c
@@ -0,0 +1,74 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   argument_d.c                                       :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: swofferh <swofferh@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2020/01/08 22:26:52 by swofferh       #+#    #+#                */
+/*   Updated: 2020/03/08 22:38:16 by swofferh      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "printf.h"
+
+void	ft_p_argument(t_info *node)
+{
+	unsigned long	nbr;
+	int ox;
+	int	len;
+	int width;
+	int precision;
+
+	ox = 2;
+	width = node->width;
+	precision = node->precision;
+	nbr = (unsigned long)va_arg(node->argument, void *);
+	len = ft_lenbase(nbr, 16);
+	if (nbr == 0 && node->period == TRUE && precision == 0)
+	{
+		if (node->flag == NOFLAG  || node->flag == ZERO)
+			pt_putlen(node, ' ', width - ox);
+		pt_putstr(node, "0x");
+		if (node->flag == MINUS)
+			pt_putlen(node, ' ', width - ox);
+	}
+	else
+	{
+		if (node->flag == NOFLAG)
+			pt_putlen(node, ' ', width - ft_maxof(len, precision) - ox);
+		pt_putstr(node, "0x");
+		if (precision > len)
+			pt_putlen(node, '0', precision - len);
+		if (node->flag == ZERO)
+			pt_putlen(node, '0', width - ft_maxof(len, precision) - ox);
+		pt_puthexa(node, nbr, 'x');
+		if (node->flag == MINUS)
+			pt_putlen(node, ' ', width - ft_maxof(len, precision) - ox);
+	}
+}
+
+void	ft_no_argument(t_info *node)
+{
+	if (node->period == TRUE)
+		{
+			if (node->flag == ZERO)
+				pt_putlen(node, ' ', node->width - node->precision);
+			if (node->flag == NOFLAG)
+				pt_putlen(node, ' ', node->width - node->precision);
+			pt_putchar(node, '%');
+			if (node->flag == MINUS)
+				pt_putlen(node, ' ', node->width - node->precision);
+			
+		}
+		else if (node->period == FALSE)
+		{
+			if (node->width > 0 && node->flag == ZERO)
+				pt_putlen(node, '0', node->width - 1);
+			if (node->width > 0 && node->flag == NOFLAG)
+				pt_putlen(node, ' ', node->width - 1);
+			pt_putchar(node, '%');
+			if (node->width > 0 && node->flag == MINUS)
+				pt_putlen(node, ' ', node->width - 1);
+		}
+}
---
+++ nolibft/type_u.c
@@ -0,0 +1,110 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   argument_d.c                                       :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: swofferh <swofferh@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2020/01/08 22:26:52 by swofferh       #+#    #+#                */
+/*   Updated: 2020/03/08 22:38:16 by swofferh      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "printf.h"
+
+void	ft_u_argument(t_info *node)
+{
+	unsigned int	nbr;
+	int neg;
+	int len;
+	int width;
+	int precision;
+
+	width = node->width;
+	precision = node->precision;
+	nbr = (unsigned int)va_arg(node->argument, unsigned int);
+	len = ft_lenbase(nbr, 10);
+	neg = nbr;
+	if (node->conversion == 'u')
+	{
+		if (neg < 0 || nbr == UNS_MAX)
+		{
+			neg = nbr * -1;
+			if (node->flag == NOFLAG)
+				pt_putlen(node, ' ', width - ft_maxof(precision, 10));
+			if (precision > 10)
+				pt_putlen(node, '0', precision - 10);
+			if (node->flag == ZERO)
+				pt_putlen(node, '0', width - ft_maxof(precision, 10));
+			pt_putnbr(node, UNS_MAX - neg + 1);
+			if (node->flag == MINUS)
+				pt_putlen(node, ' ', width - ft_maxof(precision, 10));
+		}
+		else if (nbr == 0)
+		{
+			if (node->period == TRUE && precision == 0)
+				pt_putlen(node, ' ', width);
+			else
+			{
+				if (node->flag == NOFLAG)
+					pt_putlen(node, ' ', width - ft_maxof(precision, len));
+				if (node->flag == ZERO)
+				{
+					if (node->period == FALSE)
+						pt_putlen(node, '0', width - 1);
+					else
+						pt_putlen(node, ' ', width - precision);
+				}
+				if (precision > 0)
+				 	pt_putlen(node, '0', precision - 1);
+				pt_putnbr(node, nbr);
+				if (node->flag == MINUS)
+				{
+					if (node->period == FALSE)
+						pt_putlen(node, ' ', width - 1);
+					else
+						pt_putlen(node, ' ', width - precision);
+				}
+			}
+		}
+		else
+		{
+			if (!node->period)
+			{
+				if (node->flag == MINUS)
+				{
+					pt_putnbr(node, pt_putsign(node, nbr));
+					pt_putlen(node, ' ', node->width - len);
+				}
+				else if (node->flag == ZERO)
+				{
+					nbr = pt_putsign(node, nbr);
+					pt_putlen(node, '0', node->width - len);
+					pt_putnbr(node, nbr);
+				}
+				else if (node->flag == NOFLAG)
+				{
+					pt_putlen(node, ' ', node->width - len);
+					pt_putnbr(node, pt_putsign(node, nbr));
+				}
+			}
+			if (node->period == TRUE)
+			{
+				if (node->flag == ZERO || node->flag == NOFLAG)
+					pt_putlen(node, ' ', width - ft_maxof(precision, len));
+				if (precision > len)
+					pt_putlen(node, '0', precision - len);
+				pt_putnbr(node, (unsigned int)nbr);
+				if (node->flag == MINUS)
+				{
+					if (width < precision)
+						pt_putlen(node, ' ', precision - width - len);
+					else
+						pt_putlen(node, ' ', width - ft_maxof(precision, len));
+				}
+			}
+		}
+	}
+	else if (node->conversion == 'o')
+		pt_putocta(node, nbr);
+}
